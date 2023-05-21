91b85120192e11b4624c0a25a3f9fe30a5f577c1
gergo.nagy <gergo.nagy0216@gmail.com>
2023-05-21 19:57:11 +0200

    fix average_abv

--- beer_challenge/queries/beer_with_most_pairings.sql
+++ beer_challenge/queries/beer_with_most_pairings.sql
@@ -1,13 +1,13 @@
 SELECT
     b.id,
-    name,
-    tagline,
-    first_brewed,
-    abv,
-    COUNT(food_pairing) AS pairing_count,
-    AVG(abv) AS average_abv
+    b.name,
+    b.tagline,
+    b.first_brewed,
+    b.abv,
+    COUNT(fp.food_pairing) AS pairing_count,
+    AVG(b.abv) OVER () AS average_abv
 FROM beers AS b
 INNER JOIN food_pairings_pandas AS fp ON b.id = fp.id
-GROUP BY b.id, name, tagline, first_brewed, abv
+GROUP BY b.id, b.name, b.tagline, b.first_brewed, b.abv
 ORDER BY pairing_count DESC
 LIMIT 1;
